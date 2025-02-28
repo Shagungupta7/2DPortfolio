4cbbc9ca5fb4108e3f1d730d6d65d80b8913f36a
Shagun Gupta <shagungupta0307@gmail.com>
2025-02-28 21:27:35 +0530

    update

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const scaleFactor = 3;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Java, JavaScript, and Python. I've built several projects using these languages. Regardless of the tech, I just love coding! Here’s my <a href='https://github.com/Shagungupta7' target='_blank'>GitHub</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `This is my sofa, where I take breaks after long coding sessions. Whether it's watching tech YouTubers, brainstorming new project ideas, or just relaxing with some music, this is my go-to spot. I also love exploring Web3, blockchain, and new technologies.`,
+  tv: `That's my TV. I enjoy watching tech YouTubers like TheCodingSloth, Striver, and Kunal Kushwaha. Always something new to learn!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+  Contact me at shagungupta0307@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves.
+  I probably only read one of them.`,
+  exit: `If you want to exit Shagun's portfolio, just close the tab.`,
+};
