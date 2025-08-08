2494d3fd78b8b85b889f942ba4f555d7c0d2e29d
Shagun Gupta <127201788+Shagungupta7@users.noreply.github.com>
2025-08-08 17:11:12 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ export const dialogueData = {
   "sofa-table": `This is my sofa, where I take breaks after long coding sessions. Whether it's watching tech YouTubers, brainstorming new project ideas, or just relaxing with some music, this is my go-to spot. I also love exploring Web3, blockchain, and new technologies.`,
   tv: `That's my TV. I enjoy watching tech YouTubers like TheCodingSloth, Striver, and Kunal Kushwaha. Always something new to learn!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/Shagungupta7/Resume/blob/main/Shagun_resume.pdf" target="_blank">Check it out?</a>
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Shagungupta7/Resume" target="_blank">Check it out?</a>
   Contact me at shagungupta0307@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
